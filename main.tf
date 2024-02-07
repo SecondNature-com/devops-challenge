06b55e808e18a45f9de662c67cef6e083bf1dbf3
Josh <jmitchell@secondnature.com>
2024-02-07 09:30:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_instance" "zaga_challenge" {
+resource "aws_instance" "sn_challenge" {
   ami	        = "ami-085284d24fe829cd0"
   instance_type = "t2.micro"
   tags = {
-    Name = "zaga_challenge"
+    Name = "sn_challenge"
   }
 }
